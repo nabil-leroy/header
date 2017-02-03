00d028fd1827399139fbafb930aaf929943914d8
nabil-leroy <nabileroy@yahoo.fr>
2017-02-03 09:08:53 +0100

    deuxieme test

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>E2N</title>
+    <link rel="stylesheet" href="/css/master.css">
+  </head>
+  <body>
+    <header>
+    <nav>
+    <div align="center">
+        <a href=""><img class="logo" src="https://files.slack.com/files-pri/T04735J22-F3ZB58T0R/logo.jpg" alt="logoE2N"/></a>
+     </div>
+    </nav>
+    </header>
+  </body>
+</html>
