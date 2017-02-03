43c9cd681b9e632da92405e9216f084141f70a1d
Clementb <clement.benoit5@hotmail.com>
2017-02-03 08:44:24 +0100

    bouton du header

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+        <li class="menu">
+          <a href="">Actualité</a>
+        </li>
+        <li class="menu">
+          <a href="">Evenement</a>
+        </li>
+        <li class="menu">
+          <a href="">Promo</a>
+        </li>
+        <li class="menu">
+          <a href="">Connexion</a>
+        </li>
+      </ul>
+  </body>
+</html>
